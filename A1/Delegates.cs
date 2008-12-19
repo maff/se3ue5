81e0a804257394bf6850092cde7f1d46b792ca9d
Mathias Geat <mathias@ailoo.net>
2008-12-19 20:17:51 +0100

    moved delegates to separate file & added condition&action pair class

---
+++ A1/Delegates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A1
+{
+    delegate bool CounterConstraint(int value);
+    delegate void CounterAction();
+    delegate void CounterEventHandler(object o, CounterEventArgs e);
+}
