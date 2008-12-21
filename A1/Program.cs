a3bb0d9e1c7f37e7c7605a2730306f6aeb2c3126
Mathias Geat <mathias@ailoo.net>
2008-12-21 19:58:55 +0100

    .

--- A1/Program.cs
+++ A1/Program.cs
@@ -9,40 +9,40 @@ namespace A1
     {
         static void Main(string[] args)
         {
-            Counter Counter = new Counter();
+            Counter counter = new Counter();
 
             /* condition&action pairs */
-            Counter.ConditionActions.Add(new CounterConditionAction(
+            counter.ConditionActions.Add(new CounterConditionAction(
                             new CounterCondition(CounterConditions.ValueGreaterThanThousand),
                             new CounterAction(CounterActions.ValueGreaterThanThousand)));
 
-            Counter.ConditionActions.Add(new CounterConditionAction(
+            counter.ConditionActions.Add(new CounterConditionAction(
                             new CounterCondition(CounterConditions.ValueSmallerThanThousand),
                             new CounterAction(CounterActions.ValueSmallerThanThousand)));
 
-            Counter.ConditionActions.Add(new CounterConditionAction(
+            counter.ConditionActions.Add(new CounterConditionAction(
                             new CounterCondition(CounterConditions.ValueEqualsThousand),
                             new CounterAction(CounterActions.ValueEqualsThousand)));
 
             /* event handlers */
             CounterObserver observer = new CounterObserver();
-            Counter.Reset += new CounterEventHandler(observer.Reset);
-            Counter.Increment += new CounterEventHandler(observer.Increment);
-            Counter.EvenValue += new CounterEventHandler(observer.EvenValue);
+            counter.Reset += new CounterEventHandler(observer.Reset);
+            counter.Increment += new CounterEventHandler(observer.Increment);
+            counter.EvenValue += new CounterEventHandler(observer.EvenValue);
             
             /* add some data */
-            Counter.Add(50);
-            Counter.Add();
-            Counter.Add(-5);
-            Counter.Add();
-            Counter.Clear();
-            Counter.Add(25);
-            Counter.Add();
-            Counter.Add();
-            Counter.Add(1000);
-            Counter.Add(-15);
-            Counter.Add(-12);
-            Counter.Add(-225);
+            counter.Add(50);
+            counter.Add();
+            counter.Add(-5);
+            counter.Add();
+            counter.Clear();
+            counter.Add(25);
+            counter.Add();
+            counter.Add();
+            counter.Add(1000);
+            counter.Add(-15);
+            counter.Add(-12);
+            counter.Add(-225);
 
             Console.ReadLine();
         }
